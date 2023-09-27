aaf057185073dc1f881efc213d0af0db632b268c
Dan Engle <engle.68@gmail.com>
2023-09-26 23:57:56 -0500

    Initial commit from vite and added tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+import { nanoid } from 'nanoid'
+
+import Tasks from "./components/Tasks";
+import Task from "./components/Task";
+import AddTaskButton from "./components/AddTaskButton";
+
+function App() {
+  const [tasks, setTasks] = useState([])
+
+  return (
+    <>
+    <header className="text-center text-lg ">Header</header>
+    <main>
+      <Tasks>
+        <AddTaskButton />
+        {tasks.map((task) => {
+          return <Task key={nanoid} task={task} />;
+        })}
+      </Tasks>
+    </main>
+    <footer>Footer</footer>
+  </>
+  )
+}
+
+export default App
---
+++ src/components/AddTaskButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddTaskButton() {
+  return (
+    <div>AddTaskButton</div>
+  )
+}
+
+export default AddTaskButton
